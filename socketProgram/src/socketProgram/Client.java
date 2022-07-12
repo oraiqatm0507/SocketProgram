cae0799b62005d4841654b481d6db7d2046fcc63
oraiqatm <oraiqatm@mahmouds-MacBook-Pro.local>
2022-07-12 10:20:41 -0700

    main files

---
+++ socketProgram/src/socketProgram/Client.java
@@ -0,0 +1,61 @@
+package socketProgram;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Client {
+	
+	private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public static void main(String args[]) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 6666);
+        System.out.println("Client Connected to Server.");
+        Boolean exit = false;
+        String message = "";
+        String response = "";
+        while(exit == false){
+            
+            message = scanner.nextLine();
+            
+            if(message.equals(".")){
+                response = client.sendMessage(".");
+                exit = true;
+                break;
+                
+            }else{
+                response = client.sendMessage(message);
+            }
+
+               
+        }
+     
+       scanner.close();
+        
+       // String response = client.sendMessage("hello from client 1");
+        
+        System.out.println(response);
+	}
+}
---
+++ socketProgram/src/socketProgram/Server.java
@@ -0,0 +1,66 @@
+package socketProgram;
+
+import java.net.*;
+import java.io.*;
+
+
+public class Server {
+	private ServerSocket serverSocket;
+	
+	public void start(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		System.out.println("Server started...");
+		while (true) {
+			new ClientHandler(serverSocket.accept()).start();
+		}
+            
+		
+	}
+	
+	public void stop() throws IOException {
+		serverSocket.close();
+	}
+	
+	private static class ClientHandler extends Thread{
+		private Socket clientSocket;
+		private PrintWriter out;
+		private BufferedReader in;
+		
+		
+		
+		public ClientHandler(Socket socket) {
+			System.out.println("Client " + socket.toString() + " connected.");
+			this.clientSocket = socket;
+		}
+		
+		public void run()  {
+            try {
+				out = new PrintWriter(clientSocket.getOutputStream(), true);
+				in = new BufferedReader(
+			              new InputStreamReader(clientSocket.getInputStream())
+			         );
+			            
+	            String inputLine;
+	            while ((inputLine = in.readLine()) != null) {
+	                if (".".equals(inputLine)) {
+	                    out.println("Server:Good Bye Client!");
+	                    break;
+	                }
+	                System.out.println(this.clientSocket.getPort() + ": " + inputLine );
+	                out.println(inputLine);
+	            }
+	            System.out.println("Socket " + this.clientSocket.getPort() + ": disconnected.");
+	            in.close();
+	            out.close();
+	            clientSocket.close();
+	            
+	            
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+           
+    }
+	}
+	
+}
